41957e80e2900847a7596d1817c1f71d385ab700
SOMESH <87710750+somesh-jyothula@users.noreply.github.com>
2022-05-09 18:32:37 +0530

    Update md5 password converter.py

--- md5 password converter.py
+++ md5 password converter.py
@@ -1,5 +1,5 @@
 import hashlib
 print("//////////***PASSWORD CONVERTER***//////////")
-text = input("Enter the text:")
+text = input("Enter the text/Password:")
 hashing = hashlib.md5(text.encode())
 print("The value of hash is : ", hashing.hexdigest())
