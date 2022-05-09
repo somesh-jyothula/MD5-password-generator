7e4ae3e9483fc2cb6a45084e335ad20236d8fef2
SOMESH <87710750+somesh-jyothula@users.noreply.github.com>
2022-05-09 18:41:15 +0530

    Update md5 password converter.py

--- md5 password converter.py
+++ md5 password converter.py
@@ -1,5 +1,5 @@
 import hashlib
 print("//////////***PASSWORD CONVERTER***//////////")
-text = input("Enter the text/Password:")
-hashing = hashlib.md5(text.encode())
-print("The value of hash is : ", hashing.hexdigest())
+text = input("Enter the text/Password:")   #Taking Input
+hashing = hashlib.md5(text.encode())        #Encoding the input into md5 hash algorithm.
+print("The value of hash is : ", hashing.hexdigest())  #hexdigest() is used to return given string which contains only hexadecimal digits.
